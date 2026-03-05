8569598716bf70fe762d9f62dd11277b6d4a9dd0
tony9353 <tmeng1@ocdsb.ca>
2026-03-05 10:00:17 -0500

    game states and player updaating

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 import random
+import enum
 
 import menu
-import player
+from player import Player
 import gameworld
+
+class State(enum.Enum):
+    MENU = enum.auto()
+    GAME = enum.auto()
+    END = enum.auto()
 
 
 WIDTH = 540
@@ -21,16 +27,32 @@ clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 
+game_state = State.MENU
+player = None
+
 running = True
 while running:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.K_ESCAPE:
+            running = False
 
     all_sprites.update()
-
     screen.fill(BLACK)
+    
+    match game_state:
+        case State.MENU:
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                game_state = State.GAME
+                player = Player(200, 200, 1, screen)
+        case State.GAME:
+            player.updatePosition(pygame.key.get_pressed())
+            print(f"x: {player.x}, y: {player.y}")
+        case State.END:
+            pass
+
 
 
     all_sprites.draw(screen)
--- player.py
+++ player.py
@@ -3,13 +3,23 @@ import pygame
 class Player:
     x = None
     y = None
+    velocity = 0
     screen = None
     moveCooldown = None
-    def __init__(self, x, y, screen):
+
+    def __init__(self, x, y, velocity, screen):
         self.x = x
         self.y = y
+        self.velocity = velocity
         self.screen = screen
     
-    def updatePosition(events):
-        pass
+    def updatePosition(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.x -= self.velocity
+        if keys[pygame.K_UP]:
+            self.y -= self.velocity
+        if keys[pygame.K_RIGHT]:
+            self.x += self.velocity
+        if keys[pygame.K_DOWN]:
+            self.y -= self.velocity
         

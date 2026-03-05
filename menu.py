81c70c31479092bb174b14074cb95192c0333544
PandaMgn <alecxhen1005@gmail.com>
2026-03-05 10:51:31 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,16 +4,17 @@ class Menu:
     '''
     class for the screens shown during the game
     '''
-    def __init__(self, status, screen, background):
-        pygame.font.init()
-        w, h  = screen.get_size()
-        bg_img = pygame.image.load("background_image.png").convert_alpha
-        return screen.blit(bg_image, 0,0)
-    
+    def __init__(self, font, font_color=WHITE, bg_color = BLACK):
+        self.font = font
+        self.font_color = font_color
+
+    def draw(self, screen):
+        screen.fill(self.bg_color)
+
     class start_menu:
-        def __init__(self, status, screen, background):
-            bg_image = pygame.image.load
-        
+        def __init__(self, status, screen, ):
+            screen.fill((0,0,0))
+
         def draw(screen, bacbackground):
             pygame.font.init()
             w, h = screen.get_size()
@@ -22,20 +23,29 @@ class Menu:
             textRect = text.get_rect()
             textRect.center = w // 2, h // 5
 
-        def start_button(x, y, w, h):
-            x, y = screen.get_size() 
-            button_rect = pygame.Rect(x//2, y//2, w, h, "Start")           
-            for event in pygame.event.get():  
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    mousedown_pos = pygame.mouse.get_pos()
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    mouseup_pos =  pygame.mouse.get_pos()
-                    if button_rect.collidepoint(mousedown_pos) and button_rect.collidepoint(mouseup_pos):
-                        print("HHHHHH")
-                        return "Start"
-                else:
-                    None
+class Button:
+    #Cickable button Class
+    def __init__(self, text, pos, size):
+        self.text = text
+        self.rect = pygame.Rect(pos,size)
 
-    def show_screen(status):
+    def draw(self, surface):
+        #fix do not create 2nd event handler
+        '''
+        mouse down, save, pos as bool, mouse up, check if on button, shwo sprite while and hover
+        '''
+        start_down = False
+        for event in pygame.event.get():  
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mousedown_pos = pygame.mouse.get_pos()
+                if self.rect.collidepoint(mousedown_pos):
+                    return start_down == True
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouseup_pos =  pygame.mouse.get_pos()
+                if self.rect.collidepoint(mousedown_pos) and self.rect.collidepoint(mouseup_pos):
+                    print("HHHHHH")
+                    return event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
 
+        else:
+            None
 
